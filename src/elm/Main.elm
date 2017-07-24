c595860f9c00137b0742c6fded785815f6568c9a
Chris Bloom <bloomcb@gmail.com>
2017-07-24 00:02:08 -0700

    ports kind of working

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -1,28 +1,35 @@
 port module Main exposing (..)
+
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Html.Events exposing ( onClick )
-
--- component import example
---import Components.Hello exposing ( hello )
+import Html.Events exposing (onClick)
 
 
--- APP
 main : Program Never Model Msg
 main =
-  Html.program
-    { init = (initialModel, Cmd.none)
-    , view = view
-    , update = update
-    , subscriptions = subscriptions
-    }
+    Html.program
+        { init = ( initialModel, Cmd.none )
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
 
 
 -- MODEL
-type alias Model = String
+
+
+type alias Model =
+    { rows : List String
+    }
+
 
 initialModel : Model
-initialModel = "starting string"
+initialModel =
+    { rows = [ "starting string" ]
+    }
+
+
 
 -- PORTS
 
@@ -30,10 +37,8 @@ initialModel = "starting string"
 port newComment : (String -> msg) -> Sub msg
 
 
+
 --port thumbed : Model -> Cmd msg
-
-
-
 -- SUBSCRIPTIONS
 
 
@@ -41,41 +46,58 @@ subscriptions : Model -> Sub Msg
 subscriptions model =
     newComment NewComment
 
+
+
 -- UPDATE
-type Msg = NoOp | Increment | NewComment String
+
+
+type Msg
+    = NoOp
+    | Increment
+    | NewComment String
+
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  case msg of
-    NoOp -> ( model, Cmd.none)
-    Increment ->
-        ( model ++ " increment", Cmd.none )
-    NewComment comment ->
-        ( comment ++ " from NewComment", Cmd.none )
+    case msg of
+        NoOp ->
+            ( model, Cmd.none )
+
+        Increment ->
+            ( model, Cmd.none )
+
+        NewComment comment ->
+            ( model, Cmd.none )
+
 
 
 -- VIEW
 -- Html is defined as: elem [ attribs ][ children ]
 -- CSS can be applied via class names or inline style attrib
+
+
 view : Model -> Html Msg
 view model =
-    div [ class "jumbotron text-center" ][
-      img [ src "static/img/elm.jpg", style styles.img ] []                             -- inline CSS (via var)
-      , h1 [] [ text  model ]                                                                     -- ext 'hello' component (takes 'model' as arg)
-      , p [] [ text ( "Elm Webpack Starter" ) ]
-      , button [ class "btn btn-primary btn-lg", onClick (NewComment "blerp") ] [                  -- click handler
-        span[ class "glyphicon glyphicon-star" ][]                                      -- glyphicon
-        , span[][ text "FTW!" ]
-      ]
-    ]
+    div [ class "jumbotron text-center" ]
+        [ img [ src "static/img/elm.jpg", style styles.img ] [] -- inline CSS (via var)
+        , ul [] [ text (Maybe.withDefault "blerp" (List.head model.rows)) ]
+        , p [] [ text ("Elm Webpack Starter") ]
+        , button [ class "btn btn-primary btn-lg", onClick (NewComment "blerp") ]
+            [ -- click handler
+              span [ class "glyphicon glyphicon-star" ] [] -- glyphicon
+            , span [] [ text "FTW!" ]
+            ]
+        ]
+
 
 
 -- CSS STYLES
+
+
 styles : { img : List ( String, String ) }
 styles =
-  {
-    img =
-      [ ( "width", "33%" )
-      , ( "border", "4px solid #337AB7")
-      ]
-  }
+    { img =
+        [ ( "width", "33%" )
+        , ( "border", "4px solid #337AB7" )
+        ]
+    }
